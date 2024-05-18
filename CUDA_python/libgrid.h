0f1f4beb1729b46fd71ca127e3eb844c6dab39ad
saliei <saeidaliei2019@gmail.com>
2024-05-18 19:45:49 +0200

    update

--- CUDA_python/libgrid.h
+++ CUDA_python/libgrid.h
@@ -1,8 +1,6 @@
 #ifndef LIBGRID_H
 #define LIBGRID_H
 
-#include <cuComplex.h>
-
 #define IMAGE_SIZE 2048
 #define IMAGE_SIZE_HALF 1024
 #define THETA 0.0125
@@ -13,6 +11,6 @@
 #define BASELINES 351
 #define FREQUENCS 256
 
-extern "C" void gridding_cuda(cuDoubleComplex *grid, double *uvwt, cuDoubleComplex *vist, double *freq);
+extern "C" void gridding_cuda(double complex *grid, double *uvwt, double complex *vist, double *freq);
 
 #endif // LIBGRID_H
