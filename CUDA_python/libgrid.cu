589db2c03e9e38fe051fef34407ecdfa6f46636d
saliei <saeidaliei2019@gmail.com>
2024-05-18 19:55:52 +0200

    update

--- CUDA_python/libgrid.cu
+++ CUDA_python/libgrid.cu
@@ -1,4 +1,5 @@
 #include <cuda_runtime.h>
+#include <complex>
 #include <cmath>
 #include <cstdio>
 
@@ -26,7 +27,44 @@ __global__ void gridding_cuda_kernel(double *grid_real, double *grid_imag, doubl
 }
 
 
-extern "C" void gridding_cuda(double complex *grid, double *uvw_data, double complex *visibility_data, double *frequency_data) {
+#include <cuda_runtime.h>
+#include <cmath>
+#include <cstdio>
+#include <complex>
+#include "gridding.h"
+
+#define IMAGE_SIZE 2048
+#define IMAGE_SIZE_HALF 1024
+#define THETA 0.0125
+#define C 299792458
+#define THETA_OVER_C 4.16955e-11
+
+#define TIMESTEPS 512
+#define BASELINES 351
+#define FREQUENCS 256
+
+
+__global__ void gridding_cuda_kernel(double *grid_real, double *grid_imag, double *uvw_data, double *visibility_real, double *visibility_imag, double *frequency_data) {
+    int i = blockIdx.x;
+    int j = threadIdx.x;
+    int k;
+
+    for (k = 0; k < FREQUENCS; k++) {
+        double vis_real = visibility_real[(i * BASELINES * FREQUENCS) + (j * FREQUENCS) + k];
+        double vis_imag = visibility_imag[(i * BASELINES * FREQUENCS) + (j * FREQUENCS) + k];
+        double freq = frequency_data[k];
+
+        int iu = (int)round(THETA_OVER_C * uvw_data[(i * BASELINES * 3) + (j * 3) + 0] * freq);
+        int iv = (int)round(THETA_OVER_C * uvw_data[(i * BASELINES * 3) + (j * 3) + 1] * freq);
+        int iu_idx = iu + IMAGE_SIZE / 2;
+        int iv_idx = iv + IMAGE_SIZE / 2;
+
+        atomicAdd(&(grid_real[iu_idx * IMAGE_SIZE + iv_idx]), vis_real);
+        atomicAdd(&(grid_imag[iu_idx * IMAGE_SIZE + iv_idx]), vis_imag);
+    }
+}
+
+extern "C" void gridding_cuda(std::complex<double> *grid, double *uvw_data, std::complex<double> *visibility_data, double *frequency_data) {
     double *d_grid_real, *d_grid_imag;
     double *d_uvw_data;
     double *d_visibility_real, *d_visibility_imag;
@@ -42,15 +80,15 @@ extern "C" void gridding_cuda(double complex *grid, double *uvw_data, double com
     double *grid_real = (double*) malloc(IMAGE_SIZE * IMAGE_SIZE * sizeof(double));
     double *grid_imag = (double*) malloc(IMAGE_SIZE * IMAGE_SIZE * sizeof(double));
     for (int idx = 0; idx < IMAGE_SIZE * IMAGE_SIZE; ++idx) {
-        grid_real[idx] = creal(grid[idx]);
-        grid_imag[idx] = cimag(grid[idx]);
+        grid_real[idx] = std::real(grid[idx]);
+        grid_imag[idx] = std::imag(grid[idx]);
     }
 
     double *visibility_real = (double*) malloc(TIMESTEPS * BASELINES * FREQUENCS * sizeof(double));
     double *visibility_imag = (double*) malloc(TIMESTEPS * BASELINES * FREQUENCS * sizeof(double));
     for (int idx = 0; idx < TIMESTEPS * BASELINES * FREQUENCS; ++idx) {
-        visibility_real[idx] = creal(visibility_data[idx]);
-        visibility_imag[idx] = cimag(visibility_data[idx]);
+        visibility_real[idx] = std::real(visibility_data[idx]);
+        visibility_imag[idx] = std::imag(visibility_data[idx]);
     }
 
     cudaMemcpy(d_grid_real, grid_real, IMAGE_SIZE * IMAGE_SIZE * sizeof(double), cudaMemcpyHostToDevice);
@@ -66,7 +104,7 @@ extern "C" void gridding_cuda(double complex *grid, double *uvw_data, double com
     cudaMemcpy(grid_imag, d_grid_imag, IMAGE_SIZE * IMAGE_SIZE * sizeof(double), cudaMemcpyDeviceToHost);
 
     for (int idx = 0; idx < IMAGE_SIZE * IMAGE_SIZE; ++idx) {
-        grid[idx] = grid_real[idx] + I * grid_imag[idx];
+        grid[idx] = std::complex<double>(grid_real[idx], grid_imag[idx]);
     }
 
     cudaFree(d_grid_real);
@@ -81,3 +119,4 @@ extern "C" void gridding_cuda(double complex *grid, double *uvw_data, double com
     free(visibility_real);
     free(visibility_imag);
 }
+
--- CUDA_python/libgrid.h
+++ CUDA_python/libgrid.h
@@ -1,6 +1,8 @@
 #ifndef LIBGRID_H
 #define LIBGRID_H
 
+#include <complex>
+
 #define IMAGE_SIZE 2048
 #define IMAGE_SIZE_HALF 1024
 #define THETA 0.0125
@@ -11,6 +13,6 @@
 #define BASELINES 351
 #define FREQUENCS 256
 
-extern "C" void gridding_cuda(double complex *grid, double *uvwt, double complex *vist, double *freq);
+extern "C" void gridding_cuda(std::complex<double> *grid, double *uvw_data, std::complex<double> *visibility_data, double *frequency_data);
 
 #endif // LIBGRID_H
